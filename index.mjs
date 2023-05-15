31bc6addadbe9384b3ea5787eb63da3fcccfc90c
Scott Page <swpage@gmail.com>
2023-05-15 14:01:34 -0600

    updates

--- index.mjs
+++ index.mjs
@@ -11,28 +11,71 @@ const configFiles = {
 };
 
 const usage = () => {
-  console.log(configFiles.projects.items);
+  // console.log(configFiles.projects);
+  echo('Projects:');
+  for (let project in configFiles.projects) {
+    echo`  ${chalk.blue(`${project}`)}`;
+  }
+  // TODO: add project commands
+
+  echo('Commands:');
+  for (let cmd in configFiles.commands) {
+    echo`  ${chalk.blue(`${cmd}`)}`;
+  }
+};
+
+const init = () => {
+  // Copy yml files to ~/.devlocal
+  if (!fs.existsSync(baseDir)) {
+    fs.mkdirSync(baseDir);
+    for (let configFile of Object.keys(configFiles)) {
+      echo(`Creating ${baseDir}/${configFile}.yml`);
+      fs.copyFileSync(`./defaults/${configFile}.yml`, `${baseDir}/${configFile}.yml`);
+    }
+  }
+
+  // Load yml files
+  for (let configFile of Object.keys(configFiles)) {
+    configFiles[configFile] = YAML.parse(fs.readFileSync(`${baseDir}/${configFile}.yml`, 'utf8'));
+  }
 };
 
 // --------------------------------------------------------------------------------------------- //
-if (!fs.existsSync(baseDir)) {
-  fs.mkdirSync(baseDir);
-  for (let configFile of Object.keys(configFiles)) {
-    echo(`Creating ${baseDir}/${configFile}.yml`);
-    fs.copyFileSync(`./defaults/${configFile}.yml`, `${baseDir}/${configFile}.yml`);
-  }
-}
-
-for (let configFile of Object.keys(configFiles)) {
-  configFiles[configFile] = YAML.parse(
-    fs.readFileSync(`${baseDir}/${configFile}.yml`, 'utf8')
-  ).items;
-}
+init();
 
 console.log({ projects: configFiles.projects });
 const [_, cmd, project] = argv._;
-console.log({ cmd, project, argv });
+console.log({ cmd, project });
 
-if (!cmd) {
+if (!cmd && !project) {
   usage();
+  process.exit(0);
+}
+
+let selectedProject = configFiles.projects[project] || { dir: project };
+
+if (project) {
+  // Set specified environment variables
+  const envs = selectedProject?.env || {};
+  for (const [key, value] of Object.entries(envs)) {
+    await $`export env ${key}="${value}"`;
+  }
+
+  cd(`${configFiles.variables.baseDir}/${selectedProject.dir}`);
+  if (fs.existsSync('.git')) {
+    $`git branch`;
+  }
+}
+
+if (cmd !== '') {
+  // Choose project-specific command or general command
+  console.log({ selectedProject, commands: configFiles.commands });
+  const command = selectedProject.cmds?.[cmd] || configFiles.commands[cmd];
+  console.log({ command });
+  if (command) {
+    await $`${command}`;
+  } else {
+    echo(`Command not found: ${cmd}`);
+    process.exit(1);
+  }
 }
